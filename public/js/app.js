3be5f1125b31266e2b39dfddfad1ac7cca8fa090
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-03-15 12:47:05 +0900

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Clien side javascript file.");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
